e3e7cfac764d206e66ce57dfc3e9d5eff615a893
Jimmy <zhengge2012@gmail.com>
2018-10-22 21:48:14 +0800

    add readme

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # workflowdemo
 loonflow demo
 
+# workflowdemo docker
+docker pull webterminal/workflowdemo
+docker run -itd -p 80:80 8000:8000 webterminal/workflowdemo
+
+# login user and password
+loginurl: http://127.0.0.1
+username: admin
+password: password!23456
+
 # Preview
 ![demo image](./demo.gif  "demo image")
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,10 @@ trap "kill -15 -1 && echo all proc killed" TERM KILL INT
 
 if [ "$1" = "start" ]; then
 	service mysql start
+	mysql -uroot -e "CREATE DATABASE if not exists loonflownew DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
+	mysql -uroot -e "CREATE USER loonflownew@127.0.0.1 IDENTIFIED BY '123456'";
+	mysql -uroot -e "GRANT ALL PRIVILEGES ON loonflownew.* TO 'loonflownew'@'127.0.0.1';"
+	mysql --one-database loonflownew < /opt/workflowdemo/loonflow.sql
 	service redis-server start
 	service nginx start
 	service supervisor start
