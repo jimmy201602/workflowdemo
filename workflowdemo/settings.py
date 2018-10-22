25ff6385f552f148971201ed6097942cce47672c
Jimmy <zhengge2012@gmail.com>
2018-10-22 15:54:48 +0800

    update settings

--- workflowdemo/settings.py
+++ workflowdemo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q2qkf(p@x=5g1z=taj@ix^0cbdkny%ld^ehjzi6q6wfrk129^^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
