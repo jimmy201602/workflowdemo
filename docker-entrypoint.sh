cc69ef73447ac8c2f1a22eae4e00f26e956f8c30
Jimmy <zhengge2012@gmail.com>
2018-11-08 20:09:50 +0800

    modify docker entrypoint file to fix docker start bug on mac

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 trap "kill -15 -1 && echo all proc killed" TERM KILL INT
 
 if [ "$1" = "start" ]; then
+	chown -R mysql:mysql /var/lib/mysql
 	service mysql start
 	mysql -uroot -e "CREATE DATABASE if not exists loonflownew DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
 	mysql -uroot -e "CREATE USER loonflownew@127.0.0.1 IDENTIFIED BY '123456'";
